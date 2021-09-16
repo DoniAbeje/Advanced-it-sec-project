b5b37c58aa827c1d102fd470b9c2205434a83439
Doni Abeje <doni@peoplise.com>
2021-09-16 18:46:09 +0300

    implement escape function

--- src/lib/render.ts
+++ src/lib/render.ts
@@ -5,6 +5,19 @@ export const render = async (file: String, res: Response, context = null) => {
   const source = await readFile(`./src/views/${file}.handlebars`);
   const template = hb.compile(source.toString());
   const html = template(context);
-  res.write(html)
+  res.write(html);
   res.end();
 };
+
+function escape(prop: string) {
+  var tagsToReplace = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#39;",
+  };
+  return prop.replace(/[&<>]/g, function (tag) {
+    return tagsToReplace[tag] || tag;
+  });
+}
