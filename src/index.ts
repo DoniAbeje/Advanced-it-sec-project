b76be9476e9c57ce3a8b43c3ef78bf5af2c174fc
Doni Abeje <doni@peoplise.com>
2021-09-29 08:37:15 +0300

    enable https

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,17 @@ import "./lib/load-partials";
 import dotenv from "dotenv";
 import * as db from "./lib/db-connection";
 import { parse } from "./lib/parser";
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('C:\\Users\\Doni\\Desktop\\It Security Project\\src\\key.pem'),
+  cert: fs.readFileSync('C:\\Users\\Doni\\Desktop\\It Security Project\\src\\cert.pem')
+};
 
 dotenv.config();
 (async () => await db.connect())();
-const server = http.createServer(async (req, res) => {
+const server = https.createServer(options,async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const method = req.method;
   const body = await parse(req);
